50afe3e32549f4abe59874416813c0db5b351984
Md Shobahan Dewan <160313147+ShobahanDewan@users.noreply.github.com>
2024-09-11 00:05:48 +0600

    Factorial of a User's Given Number

---
+++ Factorial of a User's Given Number.py
@@ -0,0 +1,10 @@
+def factorial(n):
+if n == 0 or n == 1:
+return 1
+else:
+return n * factorial(n - 1)
+Num = int(input("Enter a Number: "))
+if Num < 0:
+print("Factorial Is Not Defined For Negative Numbers.")
+else:
+print(f"The Factorial Of {Num} is {factorial(Num)}")

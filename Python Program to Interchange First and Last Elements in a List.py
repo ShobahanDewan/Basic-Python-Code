963f4b66d6085734faec86e86d1d47e0f1ed4bf9
Md Shobahan Dewan <160313147+ShobahanDewan@users.noreply.github.com>
2024-09-12 17:18:59 +0600

    Python Program to Interchange First and Last Elements in a List

--- Python Program to Interchange First and Last Elements in a List.py
+++ Python Program to Interchange First and Last Elements in a List.py
@@ -2,6 +2,7 @@ def interchange_first_last(lst):
 if len(lst) > 1:
  lst[0], lst[-1] = lst[-1], lst[0]
     return lst
+
 lst = input("Enter The Elements : ").split()
 result = interchange_first_last(lst)
 print("List After Interchange :", result)

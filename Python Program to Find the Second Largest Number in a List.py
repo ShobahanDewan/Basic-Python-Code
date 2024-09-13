b483c48f79e413962a9daa93e391e99fafaa8cfb
Md Shobahan Dewan <160313147+ShobahanDewan@users.noreply.github.com>
2024-09-13 20:33:38 +0600

    Python Program to Find the Second Largest Number in a List.

---
+++ Python Program to Find the Second Largest Number in a List.py
@@ -0,0 +1,12 @@
+def second_largest(lst):
+  a = list(set(lst))
+     if len(a) < 2:
+        return None
+     a.sort(reverse=True)
+  return a[1]
+lst = list(map(int, input("Enter The Elements: ").split()))
+result = second_largest(lst)
+if result is None:
+    print("Does Not Have A Second Largest Element.")
+else:
+    print(f"The Second Largest Number Is: {result}")

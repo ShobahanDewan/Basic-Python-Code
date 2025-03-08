28de67ce357729c55141560289571b202f86fb35
Md Shobahan Dewan <160313147+ShobahanDewan@users.noreply.github.com>
2025-03-08 19:02:09 +0600

    Find the Area and Circumference of a Circle Using a Function

--- Find the Area and Circumference of a Circle Using a Function.py
+++ Find the Area and Circumference of a Circle Using a Function.py
@@ -2,6 +2,7 @@ def calculate_circle(r):
 a = 3.1416 * r ** 2
 c = 2 * 3.1416 * r
 return a,c
+
 r = float(input("Enter The Radius Of The Circle: "))
 a,c = calculate_circle(r)
 print(f"Area Of The Circle: {a:.2f}")
